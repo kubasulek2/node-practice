046ad4c941c80003e5f57acdcb6bf36943c25ea7
Kuba Sukowski <kubasulek2@gmail.com>
2019-11-07 12:37:16 +0100

    .env file created, dotenv installed, weather data parsed

--- src/weather-app/app.js
+++ src/weather-app/app.js
@@ -1,7 +1,11 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/92fc1c30eeeaeff7a1cc5533a13d8f95/37.8267,-122.4233';
+require('dotenv').config();
 
-request({ url: url }, (error, response) => {
-	console.log(response);
+const url = `https://api.darksky.net/forecast/${ process.env.DARK_SKY_KEY}/37.8267,-122.4233?units=si&exclude=minutely,hourly,alerts,flags`;
+
+
+request({ url: url, json: true }, (error, response) => {
+	const current = response.body.currently; 
+	const today = response.body.daily[0];
 });
