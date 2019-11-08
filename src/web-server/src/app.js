564384e09de08cdd7c5e7902c540670d6a97d361
Kuba Sukowski <kubasulek2@gmail.com>
2019-11-08 12:35:32 +0100

    templates continue

--- src/web-server/src/app.js
+++ src/web-server/src/app.js
@@ -2,21 +2,44 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+// Define paths for Express config.
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
 
-
-// set up handlebars templates - need npm i hbs first.
+// set up handlebars templates - need npm i hbs first. If only this is present
+// templates directory name must be "views" an be in the root dir!
 app.set('view engine', 'hbs');
+
+// to override this another set - this time views:
+app.set('views', viewsPath);
+
+
+
 // express.static is responsible for handling paths, so that paths in views/.hbs files stays the same as in public/.html files
 app.use(express.static(publicDirectoryPath));
 
 
 app.get('', (req, res) => {
-	res.render('index');
-})
-app.get('/weather', (req, res) => {
-	res.send('here is the weather');
+	res.render('index', {
+		title: 'Weather App',
+		name: 'Kuba'
+	});
 });
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About'
+	});
+});
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		title: 'Help',
+		help: 'helpful text'
+	});
+});
+
 
 
 app.listen(3000, () => {
